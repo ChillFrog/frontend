1b7dd24b2029bfbfb1a4801d4ac2a22d2cb1ba94
ChillFrog <kotila222@gmail.com>
2023-02-16 16:42:44 +0300

    fixed login

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import { Routes, Route, useNavigate } from "react-router";
 import Login from "./components/Login";
 import Home from "./container/Home";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { fetchUser } from "./utils/fetchUser";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+    if (!user) navigate("/login");
+  });
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_GOOGLE_API_TOKEN}>
       <Routes>
